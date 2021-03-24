5c39ed7a93c56752b0a18c6b86424c2487d5aa94
Mita Unziyah Fajrina <arinmita08@gmail.com>
2021-03-24 15:40:44 +0700

    Update web.php

--- Minggu3_Pertemuan2/web.php
+++ Minggu3_Pertemuan2/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,5 +17,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// Route::get('user', 'ManagementUserController@index');
 Route::resource('user', 'ManagementUserController');
